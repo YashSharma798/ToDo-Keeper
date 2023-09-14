1c3326dd5579a0948614b4fd3a9e77eaf49f8496
YashSharma798 <yash2k03@gmail.com>
2023-09-14 16:43:53 +0530

    update

--- ToDo.js
+++ ToDo.js
@@ -1,15 +1,18 @@
 let inputs = document.getElementById("inp");
 let text = document.querySelector(".text");
 
+document.getElementById("btn").addEventListener("click", Add);
+
 function Add(){
     if(inputs.value == ""){
         alert("Please Enter Task")
     }else{
         let newEle = document.createElement("ul");
-        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        // newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        newEle.innerHTML=`${inputs.value} <button id="btn2">X</button>`;
         text.appendChild(newEle);
         inputs.value="";
-        newEle.querySelector("i").addEventListener("click" , remove);
+        newEle.querySelector("button").addEventListener("click" , remove);
         function remove(){
             newEle.remove()
         }
